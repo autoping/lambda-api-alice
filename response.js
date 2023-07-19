226d3ebb93bc04b659eb137a18ec6365ddf55480
achunin <aleksandr.chunin@t-systems.com>
2023-07-19 16:31:44 +0300

    try to fx allowed headers

--- response.js
+++ response.js
@@ -5,8 +5,8 @@ module.exports.getResponse = function (statusCode, body) {
         statusCode: statusCode,
         headers: {
             'Access-Control-Allow-Origin': '*',
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Methods": "*"
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': '*'
         },
         body: JSON.stringify(body)
     };
